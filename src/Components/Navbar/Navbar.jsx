4d9fd3ae6769a8427a53cbc07873e6ebc85bbdea
Don-Alvin <alvindon41@gmail.com>
2022-11-17 14:38:40 +0300

    Navbar jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { images } from '../../Constants'
 import classes from './Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className={classes.navbar}>
+      <nav className={classes.nav}>
+        <div className={classes.logo}>
+          <img src={images.logo} alt="" />
+        </div>
+        <div className={classes.nav_links}>
+
+          <div className={classes.page_links}>
+            
+            <ul>
+              <li>
+                <a href="">Home</a>
+              </li>
+              <li>
+                <a href="">Career</a>
+              </li>
+              <li>
+                <a href="">Pricing</a>
+              </li>
+            </ul>
+
+          </div>
+
+          <div className={classes.login_signup}>
+            <a href="" className={classes.login}>Log in</a>
+            <a href="" className={classes.signup}>Signup</a>
+          </div>
+        </div>
+      </nav>
+    </header>
   )
 }
 
--- src/Constants/image.js
+++ src/Constants/image.js
@@ -1,6 +1,6 @@
 import book from "../assets/book.svg";
 import message from "../assets/device-message.svg";
-import industriesLogo from "../assets/industries.svg";
+import industriesLogo from "../assets/industrieslogos.svg";
 import logo from "../assets/logo.svg";
 import searchStatus from "../assets/search-status.svg";
 import userTick from "../assets/user-tick.svg";
